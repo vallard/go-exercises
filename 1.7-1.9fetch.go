b7df5975afdfec3ea2d1dc54a1b3de6022b61ba0
vallard <vallard@benincosa.com>
2015-12-17 10:38:59 -0800

    Added 1.7-1.9

---
+++ 1.7-1.9fetch.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+)
+
+func main() {
+	for _, url := range os.Args[1:] {
+
+		// make sure each arg has http:// in the front.
+		if !strings.HasPrefix(url, "http") {
+			url = "http://" + url
+		}
+
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+		//b, err := ioutil.ReadAll(resp.Body)
+
+		_, err = io.Copy(os.Stdout, resp.Body)
+		resp.Body.Close()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
+			os.Exit(1)
+		}
+		fmt.Printf("\nHTTP Response: %s\n", resp.Status)
+	}
+}
